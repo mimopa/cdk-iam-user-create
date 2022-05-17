fae3d6a467670e6f1c578a6f7beb5dbe01b47acf
mimopa <mimopapa@gmail.com>
2022-05-17 13:23:20 +0900

    Initial commit

---
+++ test/iam_user_create.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IamUserCreate from '../lib/iam_user_create-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iam_user_create-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IamUserCreate.IamUserCreateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
