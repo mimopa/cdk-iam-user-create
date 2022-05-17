e08d4c831beb964ffba7788687e2d38a1b158064
mimopa <mimopapa@gmail.com>
2022-05-17 16:43:56 +0900

    first commit

--- lib/iam_user_create-stack.ts
+++ lib/iam_user_create-stack.ts
@@ -1,16 +1,45 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, SecretValue, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { readFileSync } from "fs";
 
-export class IamUserCreateStack extends Stack {
+// Get hand on Users from file
+const handsonUsers = readFileSync('./user.txt', {
+  encoding: "utf8",
+}).toString().split("\n");
+
+export class IamUserCreateStack extends Stack
+{
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Get existing group(arnはパラメータストアから取得する)
+    const groupArn = StringParameter.valueForStringParameter(this, 'CabcsHandsonGroupArn');
+    const handsonGroup = iam.Group.fromGroupArn(this, 'Group', groupArn);
+    
+    // Get existing policy
+    const loggingManagedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(
+      'IAMUserChangePassword',
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IamUserCreateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Get default password(arnはパラメータストアから取得する)
+    const secretArn = StringParameter.valueForStringParameter(this, 'SecretPasswordArn');
+    const secret = secretsmanager.Secret.fromSecretAttributes(this, 'handsonUserPassword', { secretPartialArn: secretArn });
+    
+    // Create Handson User
+    handsonUsers.forEach(handsonUser =>
+    {
+      new iam.User(this, handsonUser, {
+        userName: handsonUser,
+        groups: [handsonGroup],
+        managedPolicies: [loggingManagedPolicy],
+        password: SecretValue.plainText(secret.secretValueFromJson('iam_default_password').toString()),
+        passwordResetRequired: true,
+      })
+
+    });
+
   }
 }
